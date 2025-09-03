64491a4d3cd537aa240570111ef4e2a552590f9f
wowskii <108391629+wowskii@users.noreply.github.com>
2025-09-03 16:07:37 +0200

    fixed timezone issue

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import json
 from ics import Calendar, Event
 import html
+from datetime import datetime
+from zoneinfo import ZoneInfo  # Python 3.9+
+import tzdata
 
 
 
@@ -14,12 +17,15 @@ def dict_to_ics(input_file, output_file):
     
     # Create a new calendar
     calendar = Calendar()
+    paris_tz = ZoneInfo("Europe/Paris")
     
     for item in data:
+        if "description" not in item or "eventCategory" not in item or not item.get("start") or not item.get("end"):
+            continue  # skip incomplete entries
         event = Event()
         event_data = item["description"].split("\r\n\r\n<br />\r\n\r\n")
         event_data = [html.unescape(x.replace("<br />", " ")) for x in event_data]
-        if item["eventCategory"][-3:-1] == "TD" or item["eventCategory"][-3:-1] =="TP" or item["eventCategory"][-3:-1] =="CM":
+        if item["eventCategory"][-3:-1] in ("TD", "TP", "CM"):
             event.name = item["eventCategory"][-3:-1] + " " + event_data[1]
             event.location = event_data[2].replace("<br />", " ")
             event.description = event_data[-1] + item["modules"][0]
@@ -27,9 +33,8 @@ def dict_to_ics(input_file, output_file):
             event.name = event_data[0]
             event.location = event_data[1]
             event.description = event_data[-1]
-        event.begin = item["start"]
-        event.end = item["end"]
-    
+        event.begin = datetime.fromisoformat(item["start"]).replace(tzinfo=paris_tz)
+        event.end = datetime.fromisoformat(item["end"]).replace(tzinfo=paris_tz)
         calendar.events.add(event)
     
     # Write calendar to ICS file
